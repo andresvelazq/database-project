4f4caf25cabb00100079039859e7d06685d5df51
Andres Velazquez <39769112+andresvelazq@users.noreply.github.com>
2021-11-18 17:19:19 -0500

    update email
    access db to get professor emails for emailing

--- htdocs/COP4710-Project/invitemail.php
+++ htdocs/COP4710-Project/invitemail.php
@@ -5,7 +5,14 @@ $subject = "Invite to Request Book Info";
 $message = "Professors, please use this link to log in <link>.";
 
 // word wrap just in case
-$message = wordwrap($message, 70, "\r\n);
+$message = wordwrap($message, 70, "\r\n");
 
-mail(email, $subject, $message);
+$sql = "SELECT email FROM professors";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $email = $row["email"];
+    mail($email, $subject, $message);
+}
+
 ?>
--- htdocs/COP4710-Project/remindemail.php
+++ htdocs/COP4710-Project/remindemail.php
@@ -5,7 +5,13 @@ $subject = "Deadline to Submit Book Requests";
 $message = "Professors, please remember to submit your book requests by <deadline>.";
 
 // word wrap just in case
-$message = wordwrap($message, 70, "\r\n);
+$message = wordwrap($message, 70, "\r\n");
 
-mail(email, $subject, $message);
+$sql = "SELECT email FROM professors";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $email = $row["email"];
+    mail($email, $subject, $message);
+}
 ?>
