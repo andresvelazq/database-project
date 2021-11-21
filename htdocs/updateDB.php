3d259638c2060769c40fd03b4d22f5dce26a8915
Manatsa Chiomadzi <94277576+mccollegestudent@users.noreply.github.com>
2021-11-21 01:31:05 -0500

    Update updateDB.php
    so just added a bit more restrictions before update prevent empty inputs. After update pages go back to staffManageAcc the update will be visible on the table

--- htdocs/updateDB.php
+++ htdocs/updateDB.php
@@ -15,17 +15,21 @@
         $sadmin = $_POST["inSadmin".$index];
       
         // function to update DB.
-        $update = "UPDATE staff SET fname = '$fname', lname = '$lname',
-                    email = '$email', password = '$password', sadmin = '$sadmin' 
-                    WHERE sid = '$index'";
-
+      
+        if(empty($sid) ||  empty($fname) || empty($lname) || empty($email) || empty($password) || empty($sadmin) ){
+          header("Location: staffManageAcc.php");//no update if fields all/some are empty
+          exit();      
+      }
+        $update = "UPDATE staff SET fname = '$fname', lname= '$lname', email= '$email', password= '$password', sadmin = '$sadmin' WHERE sid = '$index'";
         // prepare statement
         $stmt = $conn->prepare($update);
         //execute query
         $stmt->execute();
         
         // print staff table.
-        printStaff($conn);
+       // printStaff($conn);
+       header("Location: staffManageAcc.php");//stays at staffpage after updates
+       exit();
 
     
     ?>
