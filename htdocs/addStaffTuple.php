af0a1048e2213f5a222750315d0a942ad568c067
Manatsa Chiomadzi <94277576+mccollegestudent@users.noreply.github.com>
2021-11-21 01:33:38 -0500

    Update addStaffTuple.php

--- htdocs/addStaffTuple.php
+++ htdocs/addStaffTuple.php
@@ -15,23 +15,14 @@
       $password = $_POST["inPass".$index];
       $sadmin = $_POST["inSadmin".$index];
       
-      echo 'Index: ' .$index;
-      echo 'SID: ' .$sid;
-      echo 'Fname: ' .$fname;
-      echo 'Lname: ' .$lname;
-      echo 'Email: ' .$email;
-      echo 'Password: ' .$password;
-      echo 'sadmin: ' .$sadmin;
-      echo '';
-
       $add = "INSERT INTO staff (`sid`, `fname`, `lname`, `email`, `password`, `sadmin`) VALUES ('$sid', '$fname', '$lname', '$email','$password','$sadmin')";
       $stmt = $conn->prepare($add);
       //execute query
       $stmt->execute();
       
       // print staff table.
-      printStaff($conn);
-              header("Location: staffManageAcc.php");
+        printStaff($conn);
+              header("Location: staffManageAcc.php");//goes back to staffmenutacc after addition the change is visible 
         exit();
     ?>
   </body>
