6f55bc86b4978e229f4f1dab2b09bdd8699bd6a5
Andres Velazquez <39769112+andresvelazq@users.noreply.github.com>
2021-12-06 21:16:17 -0500

    Update staffInvite.php
    button to go back

--- htdocs/staffInvite.php
+++ htdocs/staffInvite.php
@@ -11,3 +11,16 @@ foreach ($conn->query($query) as $row) {
     mail($row['email'], $subject, $message);
 }
 ?>
+
+<html>
+    <title>Invitations</title>
+    <h1>Invitations</h1>
+    <body>
+        <div>
+            <form action = "staffMenu.php" method = "post">
+                <div>Invitations successfuly sent!</div>
+                <input type="submit" value = "Back" name = "backBtn"/>
+            </form>
+        </div>
+    </body>
+</html>
